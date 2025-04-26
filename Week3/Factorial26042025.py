2dd2737af70be342948710c60e1118a4596368ca
mtenorio50 <mtenorio5001@gmail.com>
2025-04-26 17:31:13 +1200

    Delete Factorial26042025.py

--- Week3/Factorial26042025.py
+++
@@ -1,9 +0,0 @@
-def factorial(n):
-    result = 1  # Initialize result to 1
-    for i in range(1, n + 1):  # Loop from 1 to n
-        result *= i  # Multiply result by i
-    return result  # Return the final result
-
-
-num = 5
-print(f"Factorial of {num} is {factorial(num)}")
