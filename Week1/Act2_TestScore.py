6e2bd105c1919ac52a33eea7b9a28cb091cbf7eb
mtenorio50 <mtenorio5001@gmail.com>
2025-04-26 19:19:11 +1200

    Update Act2_TestScore.py

--- Week1/Act2_TestScore.py
+++ Week1/Act2_TestScore.py
@@ -10,15 +10,15 @@ scores = np.array([
     [90, 93, 94],
     [75, 80, 70]
 ])
-
+# Avg of each student
 row_avg = np.mean(scores, axis=1)
 print(f"Student avg of each student {row_avg}")
-
+# Avg of each subject
 col_avg = np.mean(scores, axis=0)
 print(f"Avg of subjects: {col_avg}")
-
+# Index of highest score
 highest_index = np.argmax(row_avg)
 print(f"Student with highest score is student {highest_index}")
-
+# +5 points on the 3rd subject
 new_score = scores[:, 2] + 5
 print(f"New score of the 3rd subject {new_score}")
