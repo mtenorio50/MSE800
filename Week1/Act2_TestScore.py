6dfec4f232060d35173e4ebc5cc5479d3beedea9
mtenorio50 <mtenorio5001@gmail.com>
2025-04-26 17:42:59 +1200

    Create Act2_TestScore.py

---
+++ Week1/Act2_TestScore.py
@@ -0,0 +1,16 @@
+# You are given the test scores of 5 students across
+# 3 subjects in a 2D Numpy array. Each row represents
+# a student, and each collumn a subject
+
+import numpy as np
+scores = np.array([
+    [78, 85, 90],
+    [88, 79, 92],
+    [84, 76, 88],
+    [90, 93, 94],
+    [75, 80, 70]
+])
+
+# Calculate and print the avg score of each student
+
+print(f"The avg score is: ")
