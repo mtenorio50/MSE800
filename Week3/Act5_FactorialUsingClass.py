1973f94ad01eb2e70807dbd2b6228da7092181ac
mtenorio50 <mtenorio5001@gmail.com>
2025-04-27 12:14:24 +1200

    Create Act5_FactorialUsingClass.py

---
+++ Week3/Act5_FactorialUsingClass.py
@@ -0,0 +1,16 @@
+# Using class write a code for factorial
+class factorial:  # Define a class named factorial
+    def __init__(self, n):
+        self.value = n  # Constructor method
+
+    def number(n):  # Define a static method named number calculate factorial
+        result = 1  # Initialize result to 1
+        for i in range(1, n + 1):  # Loop from 1 to n
+            result *= i  # Multiply result by i
+        return result  # Return the final result
+
+
+num = int(input("Enter a number: "))  # Get user input
+# Call the factorial class method to calculate factorial
+ans = factorial.number(num)
+print(f"Factorial of {num} is {ans}")
