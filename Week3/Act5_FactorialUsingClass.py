9880efb5490b029e637e0623f5bbd8f68d33cf51
mtenorio50 <mtenorio5001@gmail.com>
2025-04-27 13:00:03 +1200

    Update Act5_FactorialUsingClass.py

--- Week3/Act5_FactorialUsingClass.py
+++ Week3/Act5_FactorialUsingClass.py
@@ -6,8 +6,20 @@ class factorial:  # Define a class named factorial
             result *= i  # Multiply result by i
         return result  # Return the final result
 
+    def prime(p):
+        if num > 1:
+            for i in range(2, p):  # Check for prime numbers
+                if (num % i) == 0:  # If num is divisible by i, it is not prime
+                    print(p, "is not a prime number")
+                    break
+            else:
+                print(p, "is a prime number")
+        else:
+            print(p, "is not a prime number")
+
 
 num = int(input("Enter a number: "))  # Get user input
-# Call the factorial class method to calculate factorial
+# Call the prime method to check if the number is prime
+print(factorial.prime(num))
 obj = factorial()  # Create an instance of the factorial class
 print(f"Factorial of {num} is {obj.number(num)}")
