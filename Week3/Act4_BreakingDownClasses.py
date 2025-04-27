3aa7ec1f5144a398b0bd5b474929a124ab8e551a
mtenorio50 <mtenorio5001@gmail.com>
2025-04-27 12:14:21 +1200

    Create Act4_BreakingDownClasses.py

---
+++ Week3/Act4_BreakingDownClasses.py
@@ -0,0 +1,7 @@
+# Breakdown automated attendance system into classes
+# 1. User class (for user details and authentication)
+# 2. Attendance class (for marking and managing attendance)
+# 3. Class class (for managing class details)
+# 4. Course class (for managing course details)
+# 5. Notification class (for sending alerts and notifications)
+# 6. UserRole class (for managing user roles and permissions)
