2169b036e6921abab6b0a5065b0246abc4107298
mtenorio50 <mtenorio5001@gmail.com>
2025-04-27 12:14:17 +1200

    Create Act2_FactorialusingFunction.py

---
+++ Week3/Act2_FactorialusingFunction.py
@@ -0,0 +1,10 @@
+#Factorial using funcion
+def factorial(n):
+    result = 1  # Initialize result to 1
+    for i in range(1, n + 1):  # Loop from 1 to n (inclusive)
+        result *= i  # Multiply result by i
+    return result  # Return the final result
+
+
+num = 5  # Get user input
+print(f"Factorial of {num} is {factorial(num)}")
