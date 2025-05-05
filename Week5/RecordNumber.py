20027c7eba40a8a2c5dbd82adc72cc44f07e2b08
mtenorio50 <mtenorio5001@gmail.com>
2025-05-06 08:04:53 +1200

    Update RecordNumber.py

--- Week5/RecordNumber.py
+++ Week5/RecordNumber.py
@@ -1,19 +1,21 @@
 import pandas as pd
 
+
 class RecordCount:
     def __init__(self, file_path):
         self.file_path = file_path
 
-    def get_record_count(self):
+    def GetRecordNumber(self):
         df = pd.read_parquet(self.file_path)
         return len(df)
+
 
 def main():
     file_path = 'D:\\YooBee\\HelloWorld\\MSE800\\Week5\\Sample-1m.parquet'
     record_counter = RecordCount(file_path)
-    count = record_counter.get_record_count()
+    count = record_counter.GetRecordNumber()
     print(f'The number of records in the dataset is: {count}')
+
 
 if __name__ == "__main__":
     main()
-
