5cc8623bc18ae243fd6db90fe75f564da3c80383
mtenorio50 <mtenorio5001@gmail.com>
2025-05-31 09:20:32 +1200

    Update FactoryDesign.py

--- Week7/FactoryDesign.py
+++ Week7/FactoryDesign.py
@@ -1,53 +1,66 @@
 from abc import ABC, abstractmethod
- 
-class Factory(ABC):
-   
-    @abstractmethod
+
+
+class Factory(ABC):  # Factory is an abstract class that defines the create_product method
+    # ABC is a class that defines the abstract base class
+
+    @abstractmethod  # abstractmethod is a class that defines the abstract method. it is used to enforce the subclasses to implement the method
     def create_product(self, kind=None):
         pass
- 
+
+
+# AnimalFactory is a concrete class that implements the create_product method
 class AnimalFactory(Factory):
     def __init__(self):
         pass
- 
+
     def create_product(self, kind=None):
         if kind == "dog":
             animal = Dog()
         elif kind == "cat":
             animal = Cat()
- 
+
         return animal
- 
+
+
+# DogFactory is a concrete class that implements the create_product method
 class DogFactory(Factory):
-   
+
     def create_product(self, kind=None):
-        return Dog()
- 
+        return Dog()  # fixed the issue by returning the Dog object instead of passing it as an argument
+
+
+# CatFactory is a concrete class that implements the create_product method
 class CatFactory(Factory):
-   
+
     def create_product(self, kind=None):
-        return Cat()
- 
-class Animals(ABC):
- 
+        return Cat()  # fixed the issue by returning the Cat object instead of passing it as an argument
+
+
+class Animals(ABC):  # Animals is an abstract class that defines the run method
+
     @abstractmethod
     def run(self):
         pass
- 
-class Dog(Animals):
- 
+
+
+class Dog(Animals):  # Dog is a concrete class that implements the run method
+
     def run(self):
         print(f"I'm a Dog, I can run!!")
- 
- 
-class Cat(Animals):
+
+
+class Cat(Animals):  # Cat is a concrete class that implements the run method
     def __init__(self):
         pass
- 
+
     def run(self):
         print(f"I'm a Cat, I can run!!")
- 
+
+
 # client
 factory = DogFactory()
+# dog = Dog()
 dog = factory.create_product()
+
 dog.run()
